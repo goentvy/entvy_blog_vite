0ce485ac9b93bb7b6d3fd94d58d8980ed5a1c822
goentvy <entvyv@gmail.com>
2025-07-29 11:27:23 +0900

    Laside change

--- src/components/Laside.jsx
+++ src/components/Laside.jsx
@@ -13,8 +13,8 @@ function Laside() {
     return (
         <aside className="l_aside_wrap flex align-center justify-left items-center">
             <ul className="l_aside_list">
-                <Sidelink url="https://www.youtube.com/" label="Youtube →" />
-                <Sidelink url="https://goentvy.github.io/html2507/" label="개인과제 →" />
+                <Sidelink url="https://goentvy.github.io/html2507/" label="HTML+CSS 과제 →" />
+                <Sidelink url="https://goentvy.github.io/js2507/" label="Javascript 과제 →" />
                 <Sidelink url="https://skc4365.github.io/web2507/" label="수강생 과제 →" />
                 <Sidelink url="/entvy_blog_vite/filter" label="검색어필터 →" target="_self" />
                 <Sidelink url="/entvy_blog_vite/gallery" label="갤러리 →" target="_self" />
