6151e65e1b91f0d8cd51667f52e1942ab4c0628e
goentvy <entvyv@gmail.com>
2025-08-01 17:23:16 +0900

    contact list style add

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,10 +27,11 @@ function Contact() {
     }
 
     async function contactSubmit() {
+        if(name === '' || pwd === '' || textarea === '') return alert('이름, 비밀번호, 내용을 모두 넣어주세요.')
         const { data, error } = await supabase.from('contact').insert([
             { username: name, password: pwd, content: textarea }
         ]);
-        if(error) console.error('INSERT Error', error);
+        if(error) console.error('INSERT Error', error, data);
     }
 
     useEffect(() => {
@@ -44,11 +45,11 @@ function Contact() {
                 <form action="#">
                     <div className="contact_form">
                         <div className="contact_input">
-                            <p><input type="text" placeholder="이름" onChange={nameValue}/></p>
-                            <p><input type="password" placeholder="비밀번호" onChange={pwdValue} /></p>
+                            <p><input type="text" name="name" id="name" placeholder="이름" onChange={nameValue} autoComplete="off" /></p>
+                            <p><input type="password" name="pwd" id="pwd" placeholder="비밀번호" onChange={pwdValue} autoComplete="off" /></p>
                         </div>
                         <div className="contact_textarea">
-                            <textarea name="comment" id="myTextarea" placeholder="내용을 작성해주세요." onChange={textareaValue}></textarea>
+                            <textarea name="textarea" id="textarea" placeholder="내용을 작성해주세요." onChange={textareaValue} autoComplete="off"></textarea>
                         </div>
                         <div className="contact_btn">
                             <button type="submit" onClick={contactSubmit}>보내기</button>
@@ -57,11 +58,25 @@ function Contact() {
                 </form>
             </div>
             <div className="contact_list">
-                <ul>
+                <div className="contact_list_box">
                     {contactDatas.map((contactData) => (
-                        <li key={contactData.id}>{contactData.content}</li>
+                        <div className="contact_list_wrap" key={contactData.id}>
+                            <div className="contact_list_header">
+                                <div className="contact_list_author">
+                                    {contactData.username}
+                                </div>
+                            </div>
+                            <p className="contact_list_content">
+                                {contactData.content}
+                            </p>
+                            <div className="contact_list_footer">
+                                <div className="contact_list_footer_box">
+                                    {contactData.created_at}
+                                </div>
+                            </div>
+                        </div>
                     ))}
-                </ul>
+                </div>
             </div>
         </>
     );
